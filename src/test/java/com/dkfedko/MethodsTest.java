c1fc06b0d1d37e92885312b9adde38734909de1e
DKfedko <dkfedko@yahoo.com>
2025-07-13 04:35:01 -0400

    added tests to see if int array converts to char aaray and 3 tests to get max or 2 - 5

--- src/test/java/com/dkfedko/MethodsTest.java
+++ src/test/java/com/dkfedko/MethodsTest.java
@@ -7,21 +7,22 @@ import main.java.com.dkfedko.Methods;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MethodsTest {
-    private final Methods methods = new Methods();
+    private Methods methods = new Methods();
 
     @Test
     void shouldPrintArrayElements() {
-        //prepare
-        char[] symbols = new char[] {'D','a','n','y','l','o'};
+        //arrange
+        char[] symbols = new char[]{'D', 'a', 'n', 'y', 'l', 'o'};
         String expectedOutput = "D\n" +
-                                "a\n" +
-                                "n\n" +
-                                "y\n" +
-                                "l\n" +
-                                "o\n";
+                "a\n" +
+                "n\n" +
+                "y\n" +
+                "l\n" +
+                "o\n";
 
         // capture System.out
         PrintStream originalOut = System.out;
@@ -36,5 +37,68 @@ class MethodsTest {
 
         // assert
         assertEquals(expectedOutput, outContent.toString());
+    }
+
+    @Test
+    void shouldConvertIntToChar() {
+
+        //arrange
+        int[] java = new int[]{106, 97, 118, 97};
+        char[] expectedCharArray = {'j', 'a', 'v', 'a'};
+
+        //act
+        char[] actualCharArray = methods.displayResult(java);
+
+        //assert
+        assertArrayEquals(expectedCharArray, actualCharArray);
+    }
+
+    @Test
+    void shouldReturnMaxOfTwoInt() {
+
+        //arrange
+        int a = 5;
+        int b = 6;
+        int actual = 6;
+
+        //act
+        int expected = methods.maxOfTwo(a, b);
+
+        //asset
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void shouldReturnMaxOfThree(){
+
+        //arrange
+        int a = 3;
+        int b = 5;
+        int c = 8;
+        int actual = 8;
+
+        //act
+        int expected = methods.maxOfThree(a,b,c);
+
+        //assert
+        assertEquals(actual, expected);
+    }
+    @Test
+    void shouldReturnMaxOfFive(){
+
+        //arrange
+        int size = 18;
+        int topEdge = 16;
+        int bottomEdge = 3;
+        int length =12;
+        int weight = 74;
+
+        int actual = 74;
+
+        //act
+        int expected = methods.maxOfFive(size, topEdge, bottomEdge, length, weight);
+
+        //assert
+        assertEquals(expected, actual);
     }
 }
